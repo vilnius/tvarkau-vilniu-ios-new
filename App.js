61049d0e01460ca4a201b94ebbb61d225d702c03
Tadas Krivickas <tadas.krivickas@gmail.com>
2018-07-06 16:10:45 +0300

    Configure flow

--- App.js
+++ App.js
@@ -1,23 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// @flow
+import * as React from 'react';
+import Root from './src/Root';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => <Root />;
+export default App;
---
+++ src/Root.jsx
@@ -0,0 +1,25 @@
+// @flow
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Root extends React.Component<*> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
