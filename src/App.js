9101760072c5a113bcbff641a44dd0e06da89589
Tadas Krivickas <tadas.krivickas@gmail.com>
2018-10-27 17:20:39 +0300

    Fetch issue list from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // @flow
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { listIssues } from './api/legacyApi';
+import type { APIIssue } from './api/types';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,11 +13,27 @@ const styles = StyleSheet.create({
   },
 });
 
+type State = {
+  issues?: APIIssue[],
+};
+
 // eslint-disable-next-line react/prefer-stateless-function
-export default class App extends React.Component<*> {
+export default class App extends React.Component<*, State> {
+  state = {
+    issues: [],
+  };
+
+  async componentDidMount() {
+    const issues = await listIssues();
+    this.setState({ issues });
+  }
+
   render() {
+    const { issues } = this.state;
+
     return (
       <View style={styles.container}>
+        {(issues || []).map(issue => (<Text> {issue.description}</Text>))}
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
---
+++ src/api/legacyApi.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/prefer-default-export */
+// @flow
+import type { APIIssue } from './types';
+
+const LEGACY_API_URL = 'https://tvarkaumiesta.lt/mob_api/server.php';
+
+export const listIssues = async (): Promise<APIIssue[]> => {
+  try {
+    const response = await fetch(LEGACY_API_URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        method: 'get_problems',
+        id: 6,
+        params: {
+          start: 0,
+          limit: 10,
+        },
+      }),
+    });
+    const json = await response.json();
+    return json.result;
+  } catch (error) {
+    return [];
+  }
+};
