15beee94aa6bac9606968d05054e53c8f0ba38bc
Tadas Krivickas <tadas.krivickas@gmail.com>
2018-10-28 13:33:57 +0200

    Make app bar so the list doesn't clash with ios status bar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { StyleSheet, View } from 'react-native';
 import IssueListScreen from './issue-list/IssueListScreen';
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  appBar: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#f9f9f9',
   },
 });
 
@@ -16,7 +21,8 @@ const styles = StyleSheet.create({
 export default class App extends React.Component<*> {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.appContainer}>
+        <View style={styles.appBar} />
         <IssueListScreen />
       </View>
     );
